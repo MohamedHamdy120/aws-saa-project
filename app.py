b14a12ae871daa9bd59525b4b1c89d6fbae90b07
Mohamed Hamdy <mohamedhamdy120@hotmail.com>
2026-09-06 16:58:17 +0300

    Phase3: Containerized guestbook app with flask,sqlAlchemy and docker, tested locally

---
+++ app.py
@@ -0,0 +1,75 @@
+from dotenv import load_dotenv
+import os
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask import Flask, request, jsonify,render_template
+
+load_dotenv()
+DATABASE_URL=os.getenv("DATABASE_URL")
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+class Message(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100),nullable=False)
+    message=db.Column(db.Text, nullable=False)
+    timestamp=db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages=Message.query.order_by(Message.timestamp.desc()).all()
+    result=[]
+    for msg in messages:
+        result.append(
+            {
+                "id":msg.id,
+                "name":msg.name,
+                "message":msg.message,
+                "timestamp":msg.timestamp.isoformat()
+            }
+        )
+    return jsonify(result)
+
+@app.route('/messages',methods=['POST'])
+def add_message():
+    data=request.get_json()
+    if not data:
+        return jsonify({"error":"Invalid request"}),400
+    
+    name=data.get("name")
+    message=data.get("message")
+
+    if not name or not message:
+        return jsonify({"error":"name and messages fields are required"}),400
+    
+
+    new_message=Message(name=name,message=message)
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify(
+        {
+            "id":new_message.id,
+            "name":new_message.name,
+            "message":new_message.message,
+            "timestamp":new_message.timestamp.isoformat()
+        }
+    ),201
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
